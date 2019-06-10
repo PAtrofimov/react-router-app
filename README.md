4383d54d83578563aeac84a11b58f0aae335928a
Petr Trofimov <trop@abmcloud.com>
2019-06-10 16:23:19 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Кузница проекта "Без воды". День #2
+
+На занятии будет разработано приложение, которое поддерживает роутинг (сделаем самописный React-router 4) и асинхронную загрузку данных с [MockAPI](http://www.mockapi.io/)
+
+[как выглядит задание](https://gifs.com/gif/2-VPy1Dz)
+
+![gif задания](https://j.gifs.com/VPy1Dz.gif)
+
+## Что будет на занятии:
+
++ создание Route и Link компонентов, по аналогии с React-Router 4
++ асинхронная загрузка данных
++ работа с нативными вещами: window.location, browserHistory API
++ работа с async/await
+
+---
+
+Расписания вебинаров и прочая полезная информация:
++ [t.me/maxpfrontend](https://t.me/maxpfrontend)
++ [vk/maxpfrontend](http://vk.com/maxpfrontend)
++ [twitter](https://twitter.com/MaxPatsiansky)
---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Switch, Route, Link} from 'react-router-dom'
+import LinkBtn from './components/LinkBtn'
+import Home from './components/Home'
+import Epic from './components/Epic'
+import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const App = () => (
+    <Router> 
+    <div>
+      <header className="header">
+        <div className="top-menu">
+                   <ul>
+          <li>
+            <Link to="/">Главная</Link>
+          </li>
+          <li>
+            <Link to="/epic">Эпопея</Link>
+          </li>
+          <li>
+            <Link to="/about">О приложении</Link>
+          </li>
+        </ul>
+
+        </div>
+      </header>
+
+      <hr />
+      <div className="content">
+       
+        <Route exact path="/" component={Home} />
+        <Route path="/epic" component={Epic} />
+        <Route path="/about" render={() => <p>Сделано на Кузнице #2</p>} />
+        
+      </div>
+    </div>
+    </Router> 
+)
+
+export default App
