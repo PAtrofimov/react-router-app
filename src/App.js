69a0c93fa39025de8a0bf6c6a89b9631e460c84a
Peter Trofimov <ptrofimov7@gmail.com>
2020-12-20 13:04:10 +0200

    added gh-pages

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import React from 'react'
-import { Switch, Route, Link} from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
 import Home from './components/Home'
 import Epic from './components/Epic'
 import './App.css'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const App = () => (
-    <Router> 
+  <Router>
     <div>
       <header className="header">
         <div className="top-menu">
-                   <ul>
-          <li>
-            <Link to="/">Главная</Link>
-          </li>
-          <li>
-            <Link to="/epic">Эпопея</Link>
-          </li>
-          <li>
-            <Link to="/about">О приложении</Link>
-          </li>
-        </ul>
-
+          <ul>
+            <li>
+              <Link to="/">Главная</Link>
+            </li>
+            <li>
+              <Link to="/epic">Эпопея</Link>
+            </li>
+            <li>
+              <Link to="/about">О приложении</Link>
+            </li>
+          </ul>
         </div>
       </header>
 
       <hr />
       <div className="content">
-       
         <Route exact path="/" component={Home} />
         <Route path="/epic" component={Epic} />
-        <Route path="/about" render={() => <p>Сделано на Кузнице #2</p>} />
-        
+        <Route
+          path="/about"
+          render={() => <p>Автор: Петр Трофимов. Дата создания: 2019 год</p>}
+        />
       </div>
     </div>
-    </Router> 
+  </Router>
 )
 
 export default App
